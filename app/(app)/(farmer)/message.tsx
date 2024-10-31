0e4dcd8d9740950ee628d24ddba0822426f4344e
Albert T. Cueme <atcueme@vauldex.com>
2024-10-31 14:22:41 +0800

    Add chat page

--- app/(app)/(farmer)/message.tsx
+++ app/(app)/(farmer)/message.tsx
@@ -1,10 +1,35 @@
-import { View } from 'react-native';
-import { Text } from 'react-native-paper'
+import { useCallback, useEffect, useState } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default function Page() {
+  const [messages, setMessages] = useState([])
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    )
+  }, [])
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Message Screen</Text>
-    </View>
+    <GiftedChat
+    messages={messages}
+    onSend={messages => onSend(messages)}
+    user={{
+      _id: 1,
+    }}
+    />
   );
 }
